b7e7d103a88189cec6e1eec661e3227fbe96149f
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-05-11 19:57:28 -0400

    Exponer un recurso en particular a traves de http GET y archivo router.php para mapear las rutas

--- server.php
+++ server.php
@@ -40,10 +40,22 @@ $books = [
 ];
 
 header('Content-Type: application/json');
+
+// Levantamos el id del recurso buscado
+$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '' ;
+
 // Generamos la respuesta asumiendo que el pedido es correcto
 switch(strtoupper($_SERVER['REQUEST_METHOD'])){
+
   case 'GET':
-    echo json_encode($books);
+    if(empty($resourceId)){
+      echo json_encode($books);
+    }else{
+      if(array_key_exists($resourceId,$books)){
+        echo json_encode($books[$resourceId]);
+      }
+    }
+    
     break;
 
   case 'POST':
@@ -54,6 +66,7 @@ switch(strtoupper($_SERVER['REQUEST_METHOD'])){
 
   case 'DELETE':
     break;
+
 }
 
 ?>
