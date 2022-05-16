02801efbf858db19ac2863954afd348eec3824e5
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-05-16 18:47:19 -0400

    envio en texto plano de credenciales

--- server.php
+++ server.php
@@ -1,4 +1,12 @@
 <?php
+
+$user = array_key_exists('PHP_AUTH_USER',$_SERVER) ? $_SERVER['PHP_AUTH_USER']: '';
+$pwd = array_key_exists('PHP_AUTH_PW',$_SERVER) ? $_SERVER['PHP_AUTH_PW']: '';
+
+if($user !== 'robert' || $pwd !== '1234'){
+  die;
+}
+
 // Definimos los recursos disponibles
 $allowedResourceTypes = [
   'books',
